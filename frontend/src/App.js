d0af5826a63f765bc67a25cf930055d484610361
GuardianBob <78508863+GuardianBob@users.noreply.github.com>
2021-11-18 14:52:10 -0800

    Basic Screen Update
    added control button placeholders and canvas placeholder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import React, { Component } from 'react' 
+import Home from './Home';
 import Modal from "./components/Modal"; 
 import axios from "axios";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
 
 //  These are necessary in order to use the proxy or forbidden errors will occur when trying to POST, Delete, etc.
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
@@ -11,149 +14,31 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      viewCompleted: false,
-      activeItem: {
-        title: "",
-        description: "",
-        completed: false
-      },
-      todoList: [],
-      modal: false,
+      
+      // modal: false,
     };
+    this.routerRef = React.createRef();
   }
   
-  componentDidMount() {
-    this.refreshList();
-  }
+  // toggle = () => {
+  //   this.setState({ modal: !this.state.modal });
+  // };
 
-  refreshList = () => {
-    axios
-      .get("/api/todos/")
-      .then((res) => this.setState({ todoList: res.data }))
-      .catch((err) => console.log(err));
-  };
-
-  toggle = () => {
-    this.setState({ modal: !this.state.modal });
-  };
-
-  //Responsible for saving the task
-  handleSubmit = (item) => {
-    this.toggle();
-
-    if (item.id) {
-      axios
-        .put(`/api/todos/${item.id}/`, item)
-        .then((res) => this.refreshList());
-      return;  
-    }
-    axios
-      .post("/api/todos/", item)
-      .then((res) => this.refreshList());
-  };
-
-  createItem = () => {
-    const item = {title: "", description: "", completed: false };
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  };
-
-  handleDelete = (item) => {
-    axios
-      .delete(`/api/todos/${item.id}/`)
-      .then((res) => this.refreshList());
-  };
-
-  editItem = (item) => {
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  };
-
-  displayCompleted = status => {
-    if (status) {
-      return this.setState({ viewCompleted: true});
-    }
-    return this.setState({ viewCompleted: false});
-  };
-
-  renderTabList = () => {
-    return (
-      <div className="my-5 tab-list">
-        <button 
-          onClick={() => this.displayCompleted(true)}
-          className={this.state.viewCompleted ? "active" : ""}
-        >
-          Complete
-        </button>
-        <button 
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "" : "active"}
-        >
-          Incomplete
-        </button>
-      </div>  
-    );
-  };
-
-  renderItems = () => {
-    const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter(
-      item => item.completed === viewCompleted
-    );
-    return newItems.map(item => (
-      <li 
-        key={item.id}
-        className="list-group-item d-flex justify-content-between align-items-center"
-      >
-        <span 
-          className={`todo-title mr-2 ${
-            this.state.viewCompleted ? "completed-todo" : ""
-          }`}
-          title={item.description}
-          >
-            {item.title}
-          </span>
-          <span>
-          <button
-            className="btn btn-secondary mr-2"
-            onClick={() => this.editItem(item)}
-          >
-            Edit
-          </button>
-          <button
-            className="btn btn-danger"
-            onClick={() => this.handleDelete(item)}
-          >
-            Delete
-          </button>
-        </span>
-      </li>
-    ));
-  };
 
   render() {
     return (
-      <main className="content">
-        <h1 className="text-white text-uppercase text-center my-4">Todo App</h1>
-        <div className="row">
-          <div className="col-md-6 col-sm-10 mx-auto p-0">
-            <div className="card p-3">
-              <div className="">
-                <button onClick={this.createItem} className="btn btn-success">Add Task</button>
-              </div>
-              {this.renderTabList()}
-              <ul className="list-group list-group-flush">
-                {this.renderItems()}
-              </ul>
-            </div>
-          </div>
-        </div>
-        {this.state.modal ? (
-          <Modal
-            activeItem={this.state.activeItem}
-            toggle={this.toggle}
-            onSave={this.handleSubmit}
-          />
-        ): null}
-      </main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+        // { {this.state.modal ? (
+        //   <Modal
+        //     activeItem={this.state.activeItem}
+        //     toggle={this.toggle}
+        //     onSave={this.handleSubmit}
+        //   />
+        // ): null} }
     )
   }
 }
