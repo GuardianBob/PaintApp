ddaef55dd1943c1cfd67f4f0074189209ced609c
GuardianBob <78508863+GuardianBob@users.noreply.github.com>
2021-11-21 21:12:33 -0800

    updated tool menu
    Cleaned up menu and added icons

---
+++ pyPaint/static/js/nav.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){  
+    $('#nav_logo').click(function(){
+        $('#nav-bar').animate({width: 'toggle'});
+        $('#nav_logo_hidden').animate({width: 'toggle'});
+    });
+    $('#nav_logo_hidden').click(function(){
+        $('#nav-bar').animate({width: 'toggle'});
+        $('#nav_logo_hidden').animate({width: 'toggle'});
+    });    
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    
+});
+
+function openNav() {
+    $("#mySideNav").width("60%");}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySideNav").style.width = "0";
+}
+
+function toggleCode(div) {
+    $(div).toggle({height: 'toggle'});
+}
