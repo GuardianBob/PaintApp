36ed1552d9995bce9c98cd5f3b2de725c2b06495
GuardianBob <78508863+GuardianBob@users.noreply.github.com>
2021-11-22 15:43:23 -0800

    Deployment

--- paintApp/settings.py
+++ paintApp/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.21.234.185']
 
 
 # Application definition
@@ -139,3 +139,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
